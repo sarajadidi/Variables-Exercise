43a8e4fc8b3dce38e703eb14215996c59be89eee
sarajadidi <sararivkajadidi@gmail.com>
2023-09-28 09:37:11 -0700

    completed the assignment

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,30 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            /* I had some errors before, but it would usually be because I forgot a quotation mark 
+             * or maybe a dollar sign or something along that line*/
+
+            string myName = "Sara";
+
+            int firstNumber = 20;
+
+            char letter= 'X';
+
+            bool myBool = true;
+
+            double myHeight = 63;
+
+            decimal myNumber = 300.00m;
+
+            string myDog = "Zeus"
+;
+            int dogAge = 2;
+
+
+            Console.WriteLine($"My dog's name is {myDog}, he is {dogAge} years old."); 
+
+
         }
     }
 }
